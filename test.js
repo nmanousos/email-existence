f1359d2267067fed43575e1406667173eb8f46e8
Nicholas Manousos <nmanousos@gmail.com>
2012-03-08 23:21:34 -0800

    changing test email

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var emailExistence = require('./lib/index.js');
 
-emailExistence.check('nmanousos@gmail.com', function(err,res){
+emailExistence.check('test@domain.com', function(err,res){
     if(!err){
         console.log('res: '+res);
     }
